de7fc5cb819cd447ba75b170107347506b766387
NatePillay <nathan94pillay@gmail.com>
2022-10-16 12:01:01 -0700

    new commit

---
+++ 0-printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _printf - function to print to stdout 
+ * @format: first var
+ * Return: chars
+ */
+
+int _printf(const char *format, ...)
+{
+	int print_char;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+
+	va_list arg_list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(arg_list, format);
+	print_char = parser(format f_list, arg_list);
+	va_end(arg_list);
+	return (print_char);
+}
